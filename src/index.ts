aae39f9d7fc378c1b44fa06f174112b834b622cc
genguxuanyin <forever_ydy@163.com>
2022-12-11 22:40:37 +0800

    done

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,18 @@
-const axios = require('axios').default;
-const cheerio = require('cheerio');
+import { fetchAll } from "./bookList";
 
-/**
- * 获取豆瓣读书网页的源代码
- */
-async function getBooksHTML() {
-  const resp = await axios.get("https://book.douban.com/latest");
-  return resp.data;
-}
-
-/**
- * 从豆瓣读书中得到一个完整的网页，并从网页中分析出书籍的基本信息，然后得到一个书籍的详情页链接数组
- */
-async function getBookLinks() {
-  const html = await getBooksHTML()
-  const $ = cheerio.load(html)
-
-  const archorElements = $('#content .grid-16-8 .chart-dashed-list .media__img a')
-
-  const links = archorElements.map((index, ele) => {
-    return ele.attribs.href
-  }).get();
-  return links;
-}
+const express = require("express");
+const app = express(); //创建一个express应用
 
 
-async function getBookDetail(detailUrl: string){
-  const resp = await axios.get(detailUrl);
-  const $ = cheerio.load(resp.data);
-  const name = $("h1").text().trim();
-  const imgurl = $("#mainpic .nbg img").attr("src");
-  const spans = $("#info span.pl");
-  const authorSpan = spans.filter((i, ele) => {
-    return $(ele).text().includes("作者");
-  });
-  const author = authorSpan.next("a").text();
-  const publishSpan = spans.filter((i, ele) => {
-    return $(ele).text().includes("出版年");
-  });
-  const publishDate = publishSpan[0].nextSibling.nodeValue.trim();
-  return {
-    name,
-    imgurl,
-    publishDate,
-    author,
-  };
-}
-
-async function fetchAll(){
-  const links = await getBookLinks()
-  const promiseArray = links.map((link) => {
-    return getBookDetail(link)
+app.get("/getBookList", async (req, res, next) => {
+  const bookList = await fetchAll()
+  res.send({
+    code: 2000,
+    data: bookList
   })
-  return Promise.all(promiseArray)
-}
+});
 
-fetchAll().then(console.log)
-
-
+const port = 5008;
+app.listen(port, () => {
+  console.log(`server listen on ${port}`);
+});
